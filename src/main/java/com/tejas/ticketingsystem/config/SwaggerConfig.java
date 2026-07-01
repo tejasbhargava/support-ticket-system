51c97d1cc52a9ddf0b12c1795081fe06f417d3f2
Tejas Bhargava <tejasbhargava9@gmail.com>
2026-07-02 01:02:33 +0530

    Fix Swagger HTTPS configuration

--- src/main/java/com/tejas/ticketingsystem/config/SwaggerConfig.java
+++ src/main/java/com/tejas/ticketingsystem/config/SwaggerConfig.java
@@ -1,5 +1,6 @@
 package com.tejas.ticketingsystem.config;
 
+import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.security.SecurityRequirement;
@@ -7,6 +8,8 @@ import io.swagger.v3.oas.models.security.SecurityScheme;
 import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import java.util.List;
 
 @Configuration
 public class SwaggerConfig {
@@ -17,23 +20,24 @@ public class SwaggerConfig {
         final String securityScheme = "bearerAuth";
 
         return new OpenAPI()
-                .addServersItem(
-                        new Server()
-                                .url("https://support-ticket-system-production-9564.up.railway.app")
-                                .description("Production")
-                )
                 .info(new Info()
                         .title("Support Ticket System API")
                         .version("1.0")
                         .description("Role-Based Support Ticket Platform"))
+                .servers(List.of(
+                        new Server()
+                                .url("https://support-ticket-system-production-9564.up.railway.app")
+                                .description("Production"),
+                        new Server()
+                                .url("http://localhost:8080")
+                                .description("Local")
+                ))
                 .addSecurityItem(new SecurityRequirement().addList(securityScheme))
-                .schemaRequirement(
-                        securityScheme,
-                        new SecurityScheme()
+                .components(new Components()
+                        .addSecuritySchemes(securityScheme, new SecurityScheme()
                                 .name(securityScheme)
                                 .type(SecurityScheme.Type.HTTP)
                                 .scheme("bearer")
-                                .bearerFormat("JWT")
-                );
+                                .bearerFormat("JWT")));
     }
 }
