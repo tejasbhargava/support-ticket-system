07cd1ca39f20b4008de3e4bedaa442070a125d02
Tejas Bhargava <tejasbhargava9@gmail.com>
2026-07-02 00:50:36 +0530

    Fix OpenAPI HTTPS server URL

--- src/main/java/com/tejas/ticketingsystem/config/SwaggerConfig.java
+++ src/main/java/com/tejas/ticketingsystem/config/SwaggerConfig.java
@@ -4,6 +4,7 @@ import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.security.SecurityRequirement;
 import io.swagger.v3.oas.models.security.SecurityScheme;
+import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -16,6 +17,11 @@ public class SwaggerConfig {
         final String securityScheme = "bearerAuth";
 
         return new OpenAPI()
+                .addServersItem(
+                        new Server()
+                                .url("https://support-ticket-system-production-9564.up.railway.app")
+                                .description("Production")
+                )
                 .info(new Info()
                         .title("Support Ticket System API")
                         .version("1.0")
